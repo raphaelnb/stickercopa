6fa11586ed8d091cb370b136389fd895f74f7263
Raphael Nobre <nobre.roots@gmail.com>
2022-10-27 23:25:05 -0300

    primeiro commit

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Image, SafeAreaView, ScrollView, TextInput, View } from 'react-native';
 
 import { Header } from '../components/Header';
+import { Camera, CameraType } from 'expo-camera'
 import { Button } from '../components/Button';
 import { PositionChoice } from '../components/PositionChoice';
 
@@ -9,7 +10,20 @@ import { styles } from './styles';
 import { POSITIONS, PositionProps } from '../utils/positions';
 
 export function Home() {
+  const [photo, setPhotoURI] = useState<null | string>(null)
+  const [hasCameraPermission, setHasCameraPermission] = useState(false)
   const [positionSelected, setPositionSelected] = useState<PositionProps>(POSITIONS[0]);
+
+  const cameraRef = useRef<Camera>(null)
+  async function handleTakePicture() {
+    const photo = await cameraRef.current.takePictureAsync()
+    setPhotoURI(photo.uri)
+  }
+
+  useEffect(() => {
+    Camera.requestCameraPermissionsAsync()
+      .then(response => setHasCameraPermission(response.granted))
+  },[])
 
   return (
     <SafeAreaView style={styles.container}>
@@ -19,8 +33,15 @@ export function Home() {
 
           <View style={styles.picture}>
 
-            <Image source={{ uri: 'https://github.com/rodrigorgtic.png' }} style={styles.camera} />
-
+            {
+              hasCameraPermission && !photo ? 
+                <Camera 
+                  ref={cameraRef}
+                  style={styles.camera}
+                  type={CameraType.front}
+                /> :
+                <Image source={{ uri: photo ? photo : 'https://github.com/raphaelnb.png' }} style={styles.camera} />
+            }
             <View style={styles.player}>
               <TextInput
                 placeholder="Digite seu nome aqui"
@@ -35,7 +56,7 @@ export function Home() {
           positionSelected={positionSelected}
         />
 
-        <Button title="Compartilhar" />
+        <Button title="Compartilhar"onPress={handleTakePicture} />
       </ScrollView>
     </SafeAreaView>
   );
